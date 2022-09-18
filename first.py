131e21e074423325d79c0dd06c60b76db55e8ddc
Aishwarya P <88570251+Aishwarya2422@users.noreply.github.com>
2022-09-18 15:03:56 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,24 @@
+# AREA OF RECTANGLE
+
+a=int(input("Enter length"))
+b=int(input("ENter breadth"))
+print('area is')
+print(a*b)
+
+
+#RANDOM NUMBER
+
+import random
+print("Random number")
+print(random.randint(0,9))
+
+
+#SWAPPING
+
+c=10
+d=30
+print(c,d)
+t=c
+c=d
+d=t
+print(c,d)
